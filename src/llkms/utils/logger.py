b8d4e39f102a49eeef340eb20ed3e533aa549f42
milosz.kucharski <miloszkucharski@gmail.com>
2025-02-17 22:40:29 +0100

    Remove console logging handler from logger setup for cleaner output

--- src/llkms/utils/logger.py
+++ src/llkms/utils/logger.py
@@ -22,12 +22,12 @@ def setup_logger():
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(file_formatter)
     
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(console_formatter)
+    # console_handler = logging.StreamHandler(sys.stdout)
+    # console_handler.setLevel(logging.INFO)
+    # console_handler.setFormatter(console_formatter)
     
     logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+    # logger.addHandler(console_handler)
     
     return logger
 
